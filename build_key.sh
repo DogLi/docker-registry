236bb652d79688ce067225ff10e9352dd1954a01
linfeng.yuan <ylf@sparkpool.com>
2018-03-21 17:23:34 +0800

    add auth

---
+++ build_key.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir conf
+openssl req -new -newkey rsa:4096 -days 365 -subj "/CN=localhost" \
+        -nodes -x509 -keyout conf/auth.key -out conf/auth.cert
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mkdir -p /opt/docker-registry
+mkdir -p /otp/docker-registry-web
